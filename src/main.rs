399e5d69c02f89bf3fa18fd89d5d122a2f971377
Martin Dahl <martin.dahl@gmail.com>
2021-07-28 00:52:32 +0200

    simplify into one realtime and one dashboard task

--- src/main.rs
+++ src/main.rs
@@ -59,165 +59,182 @@ fn read_f64(slice: &[u8]) -> f64 {
     f64::from_be_bytes(bytes)
 }
 
-async fn ur_reader(
+async fn realtime(
     driver_state: Arc<Mutex<DriverState>>,
+    mut recv: mpsc::Receiver<std::string::String>,
     dashboard_commands: mpsc::Sender<(DashBoardCommand, oneshot::Sender<bool>)>,
     joint_publisher: Publisher<sensor_msgs::msg::JointState>,
     robot_publisher: Publisher<std_msgs::msg::String>,
-    mut stream: tokio::net::tcp::OwnedReadHalf,
+    ur_address: String,
     tf_prefix: String,
 ) -> Result<(), std::io::Error> {
     let mut checking_for_1 = false;
     let mut cancelling = false;
     let mut clock = r2r::Clock::create(r2r::ClockType::RosTime).unwrap();
-    let mut ticker = 0;
+    let mut ticker = 0u32;
+
+    let mut stream;
 
     loop {
-        let mut size_bytes = [0u8; 4];
-        println!("reading...");
-        stream.read_exact(&mut size_bytes).await.expect("could not read realtime data");
-        let msg_size = u32::from_be_bytes(size_bytes) as usize;
-        println!("done reading... {} bytes", msg_size);
-
-        // need to subtract the 4 we already read
-        let mut buf: Vec<u8> = Vec::new();
-        buf.resize(msg_size - 4, 0);
-        stream.read_exact(&mut buf).await?;
-
-        if msg_size == 1116 {
-            // let time = read_f64(&buf[0..8]);
-            // println!("t: {:.1}s", time);
-            let mut joints = vec![];
-            let mut speeds = vec![];
-            for i in 0..6 {
-                let index = 248 + i * 8;
-                let joint_val = read_f64(&buf[index..index + 8]);
-                joints.push(joint_val);
-            }
-
-            for i in 0..6 {
-                let index = 296 + i * 8;
-                let joint_speed = read_f64(&buf[index..index + 8]);
-                speeds.push(joint_speed);
-            }
-            let robot_state = read_f64(&buf[808..816]).to_string();
-            // println!("robot state {:?}", robot_state);
-            let program_state = read_f64(&buf[1048..1056]).to_string();
-            // println!("program state {:?}", program_state);
-
-            let program_running = driver_state.lock().unwrap().goal.is_some();
-
-            // when we have a goal, first wait until program_state reaches "2'
-            if program_running && program_state == "2" {
-                println!("program started, waiting for finish");
-                checking_for_1 = true;
-            }
-
-            // when the program state has been "2" and goes back to
-            // "1", the goal has succeeded
-            if checking_for_1 && program_state == "1" {
-                println!("program started and has now finished");
-                // reset state machine
-                checking_for_1 = false;
-
-                // we are finished. succeed and remove the action goal handle.
-                let result_msg = ExecuteScript::Result { ok: true };
-                {
-                    let mut ds = driver_state.lock().unwrap();
-                    if let Some(mut goal) = ds.goal.take() {
-                        goal.succeed(result_msg).expect("could not set result");
-                    } else {
-                        println!("we fininshed but probably canceled the goal before...");
-                    }
-                }
-
-                // just checking...
-                assert!(speeds.iter().all(|x| x.abs() == 0.0));
-            }
-
-            // handle cancel requests
-            let is_cancelling = driver_state.lock().unwrap()
-                .goal.as_ref().map(|g|g.is_cancelling()).unwrap_or(false);
-            if !cancelling && is_cancelling {
-                    // cancel and remove goal.
-                    cancelling = true;
-                    // this starts a race between completing the motion and cancelling via stopping.
-                    // goal removal is done by the one that succeeds first.
-                    let (sender, future) = oneshot::channel();
-                    dashboard_commands.try_send((DashBoardCommand::Stop, sender)).expect("could not send...");
-
-                    let driver_state_task = driver_state.clone();
-                    tokio::spawn(async move {
-                        if future.await.expect("failed to await cancel result") {
-                            // successfully canceled.
-                            println!("cancel success...");
-                            let result_msg = ExecuteScript::Result { ok: false };
-                            if let Some(mut goal) = driver_state_task.lock().unwrap().goal.take() {
-                                goal.cancel(result_msg).expect("could not cancel goal");
-                            }
-                        } else {
-                            println!("failed to cancel... doing nothing.");
-                        }
-                    });
-                }
-
-            if !is_cancelling {
-                cancelling = false;
-            }
-
-            let rob_send = std_msgs::msg::String {
-                data: if robot_state == "1" {
-                    "normal".to_owned()
-                } else if robot_state == "3" {
-                    "protective".to_owned()
-                } else {
-                    "unknown".to_owned()
-                },
-            };
-
-            let now = clock.get_now().unwrap();
-            let time = r2r::Clock::to_builtin_time(&now);
-
-            let header = std_msgs::msg::Header {
-                stamp: time,
-                ..Default::default()
-            };
-            let to_send = sensor_msgs::msg::JointState {
-                header,
-                position: joints,
-                name: vec![
-                    format!("{}_shoulder_pan_joint", tf_prefix),
-                    format!("{}_shoulder_lift_joint", tf_prefix),
-                    format!("{}_elbow_joint", tf_prefix),
-                    format!("{}_wrist_1_joint", tf_prefix),
-                    format!("{}_wrist_2_joint", tf_prefix),
-                    format!("{}_wrist_3_joint", tf_prefix),
-                ],
-                ..Default::default()
-            };
-
-            joint_publisher.publish(&to_send).unwrap();
-
-            ticker += 1;
-            if ticker % 1000 == 0 {
-                robot_publisher.publish(&rob_send).unwrap();
-                println!("robot state {:?}", robot_state);
-                println!("program state {:?}", program_state);
+        let ret = TcpStream::connect(&ur_address).await;
+        match ret {
+            Ok(s) => {
+                println!("realtime connected");
+                stream = s;
+                // all is well!
+                break;
+            },
+            Err(e) => {
+                println!("could not connect to realtime at {}: {}", ur_address, e);
+                tokio::time::sleep(std::time::Duration::from_secs(1)).await;
             }
         }
     }
-}
 
-async fn ur_writer(
-    mut recv: mpsc::Receiver<std::string::String>,
-    mut stream: tokio::net::tcp::OwnedWriteHalf
-) -> Result<(), std::io::Error> {
     loop {
-        let data = recv.recv().await.unwrap();
+        let mut size_bytes = [0u8; 4];
 
-        println!("writing data to driver {}", data);
-        stream.write_all(data.as_bytes()).await?;
-        stream.flush().await?;
+        // we either just write, if there is an incoming script, or we just read.
+        tokio::select! {
+            data = recv.recv() => {
+                if let Some(data) = data {
+                    println!("writing data to driver {}", data);
+                    stream.write_all(data.as_bytes()).await?;
+                    stream.flush().await?;
+                }
+            }
+            _ = stream.read_exact(&mut size_bytes) => {
+                let msg_size = u32::from_be_bytes(size_bytes) as usize;
+                println!("done reading... {} bytes", msg_size);
+
+                // need to subtract the 4 we already read
+                let mut buf: Vec<u8> = Vec::new();
+                buf.resize(msg_size - 4, 0);
+                stream.read_exact(&mut buf).await?;
+
+                if msg_size == 1116 {
+                    // let time = read_f64(&buf[0..8]);
+                    // println!("t: {:.1}s", time);
+                    let mut joints = vec![];
+                    let mut speeds = vec![];
+                    for i in 0..6 {
+                        let index = 248 + i * 8;
+                        let joint_val = read_f64(&buf[index..index + 8]);
+                        joints.push(joint_val);
+                    }
+
+                    for i in 0..6 {
+                        let index = 296 + i * 8;
+                        let joint_speed = read_f64(&buf[index..index + 8]);
+                        speeds.push(joint_speed);
+                    }
+                    let robot_state = read_f64(&buf[808..816]).to_string();
+                    // println!("robot state {:?}", robot_state);
+                    let program_state = read_f64(&buf[1048..1056]).to_string();
+                    // println!("program state {:?}", program_state);
+
+                    let program_running = driver_state.lock().unwrap().goal.is_some();
+
+                    // when we have a goal, first wait until program_state reaches "2'
+                    if program_running && program_state == "2" {
+                        println!("program started, waiting for finish");
+                        checking_for_1 = true;
+                    }
+
+                    // when the program state has been "2" and goes back to
+                    // "1", the goal has succeeded
+                    if checking_for_1 && program_state == "1" {
+                        println!("program started and has now finished");
+                        // reset state machine
+                        checking_for_1 = false;
+
+                        // we are finished. succeed and remove the action goal handle.
+                        let result_msg = ExecuteScript::Result { ok: true };
+                        {
+                            let mut ds = driver_state.lock().unwrap();
+                            if let Some(mut goal) = ds.goal.take() {
+                                goal.succeed(result_msg).expect("could not set result");
+                            } else {
+                                println!("we fininshed but probably canceled the goal before...");
+                            }
+                        }
+
+                        // just checking...
+                        assert!(speeds.iter().all(|x| x.abs() == 0.0));
+                    }
+
+                    // handle cancel requests
+                    let is_cancelling = driver_state.lock().unwrap()
+                        .goal.as_ref().map(|g|g.is_cancelling()).unwrap_or(false);
+                    if !cancelling && is_cancelling {
+                        // cancel and remove goal.
+                        cancelling = true;
+                        // this starts a race between completing the motion and cancelling via stopping.
+                        // goal removal is done by the one that succeeds first.
+                        let (sender, future) = oneshot::channel();
+                        dashboard_commands.try_send((DashBoardCommand::Stop, sender)).expect("could not send...");
+
+                        let driver_state_task = driver_state.clone();
+                        tokio::spawn(async move {
+                            if future.await.expect("failed to await cancel result") {
+                                // successfully canceled.
+                                println!("cancel success...");
+                                let result_msg = ExecuteScript::Result { ok: false };
+                                if let Some(mut goal) = driver_state_task.lock().unwrap().goal.take() {
+                                    goal.cancel(result_msg).expect("could not cancel goal");
+                                }
+                            } else {
+                                println!("failed to cancel... doing nothing.");
+                            }
+                        });
+                    }
+
+                    if !is_cancelling {
+                        cancelling = false;
+                    }
+
+                    let rob_send = std_msgs::msg::String {
+                        data: if robot_state == "1" {
+                            "normal".to_owned()
+                        } else if robot_state == "3" {
+                            "protective".to_owned()
+                        } else {
+                            "unknown".to_owned()
+                        },
+                    };
+
+                    let now = clock.get_now().unwrap();
+                    let time = r2r::Clock::to_builtin_time(&now);
+
+                    let header = std_msgs::msg::Header {
+                        stamp: time,
+                        ..Default::default()
+                    };
+                    let to_send = sensor_msgs::msg::JointState {
+                        header,
+                        position: joints,
+                        name: vec![
+                            format!("{}_shoulder_pan_joint", tf_prefix),
+                            format!("{}_shoulder_lift_joint", tf_prefix),
+                            format!("{}_elbow_joint", tf_prefix),
+                            format!("{}_wrist_1_joint", tf_prefix),
+                            format!("{}_wrist_2_joint", tf_prefix),
+                            format!("{}_wrist_3_joint", tf_prefix),
+                        ],
+                        ..Default::default()
+                    };
+
+                    joint_publisher.publish(&to_send).unwrap();
+
+                    ticker += 1;
+                    if ticker % 1000 == 0 {
+                        robot_publisher.publish(&rob_send).unwrap();
+                        println!("robot state {:?}", robot_state);
+                        println!("program state {:?}", program_state);
+                    }
+                }
+            }
+        }
     }
 }
 
@@ -226,7 +243,7 @@ enum DashBoardCommand {
     Stop,
 }
 
-async fn dashboard_writer(
+async fn dashboard(
     mut recv: tokio::sync::mpsc::Receiver<(DashBoardCommand, oneshot::Sender<bool>)>,
     ur_address: String,
 ) -> Result<(), std::io::Error> {
@@ -343,22 +360,19 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         Box::new(handle_goal_cb),
     )?;
 
-    let r_shared_state = shared_state.clone();
-
-    let stream = TcpStream::connect(&ur_address).await?;
-    let (reader, writer) = stream.into_split();
+    let task_shared_state = shared_state.clone();
     tokio::spawn(async move {
-        let reader = ur_reader(
-            r_shared_state,
+        let realtime = realtime(
+            task_shared_state,
+            rx,
             tx_dashboard,
             joint_publisher,
             rob_publisher,
-            reader,
+            ur_address.to_owned(),
             tf_prefix.to_owned(),
         );
-        let writer = ur_writer(rx, writer);
-        let dashboard_writer = dashboard_writer(rx_dashboard, ur_dashboard_address.to_owned());
-        tokio::try_join!(reader, writer, dashboard_writer)
+        let dashboard = dashboard(rx_dashboard, ur_dashboard_address.to_owned());
+        tokio::try_join!(realtime, dashboard)
     });
 
     loop {
